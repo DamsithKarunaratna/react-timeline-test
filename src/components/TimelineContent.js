05f4a19a7b21b259aa14932c8cae0a9cce3a5e39
dammakaru <dammakaru@gmail.com>
2018-01-30 12:36:31 +0530

    '

--- src/components/TimelineContent.js
+++ src/components/TimelineContent.js
@@ -78,9 +78,7 @@ class TimelineContent extends Component {
                 </div>
                 
                 <Stack distribution="trailing">
-                
-                {buttonCode}
-                
+                    {buttonCode}
                 </Stack>
                    
             </div>
