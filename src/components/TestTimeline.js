2b57ee581174e4a506af0f441c6de8abfb721497
dammakaru <dammakaru@gmail.com>
2018-02-03 20:41:49 +0530

    logic for customer app otp

--- src/components/TestTimeline.js
+++ src/components/TestTimeline.js
@@ -2,6 +2,7 @@ import {Timeline, TimelineEvent} from 'react-event-timeline'
 import React, { Component } from 'react';
 import * as axios from 'axios';
 import TimelineContent from './TimelineContent';
+import { isObject } from 'util';
 
 class NewTimeline extends Component {
 
@@ -43,34 +44,65 @@ class NewTimeline extends Component {
         axios({
             method: 'get',
             //https://tracified-mock-api.herokuapp.com/Traceability_data/otp/customer-app
-            url: 'http://www.mocky.io/v2/5a6818df2d00002c3cbed081', headers: {
-                'Content-Type': 'text/plain;charset=utf-8',
+            url: 'http://www.mocky.io/v2/5a75a9b32e00004d006ab1e3', headers: {
+                'Content-Type': 'application/json;charset=utf-8',
             },
         })
             .then(response => {
-                let timeline = response.data[2];
-                let itms = response.data[2].items;
+                let timeline = response.data[0].tabs[2]
 
-                let arr = [];
+                timeline.items.map((stage, index) => {
+                    let titleText = (index+1)+". "+stage.title;
+                    let descriptionText = stage.description;
+                    var ico = (<svg height="50" width="35" >
+                                    <image width="32" height="50" xlinkHref={stage.icon}  />    
+                                </svg>);
 
-                itms.map((e,i) => {
-                    arr.push(false);
-                    return true;
-                });
-
-                this.setState({
-                    timeline: timeline,
-                    istimelineLoading: false,
-                    array: arr
+                    var stageData = stage.data;
+                    console.log("stage is: "+stage.title);
+                    Object.keys(stageData).map((key)=>{
+                        let subGroup = stageData[key];
+                        console.log("\tmain group is : "+subGroup.title);
+                        if(subGroup.hasOwnProperty("value")){
+                            console.log("\t\t"+subGroup.title+" has no grouping");
+                        }else{
+                            console.log("\t\tsubgroup is : "+ subGroup.title);
+                            Object.keys(subGroup).map((innerKey)=>{
+                                
+                                if(isObject(subGroup[innerKey])){
+                                    console.log("\t\t\tsubgroup items : "+subGroup[innerKey].title);
+                                }
+                                
+                            });
+                        }
+                        
+                        
+                    });
                     
                 });
+                // let timeline = response.data[2];
+                // let itms = response.data[2].items;
+
+                // let arr = [];
+
+                // itms.map((e,i) => {
+                //     arr.push(false);
+                //     return true;
+                // });
+
+                // this.setState({
+                //     timeline: timeline,
+                //     istimelineLoading: false,
+                //     array: arr
+                    
+                // });
             });
 
     }
 
     render(){
 
-        // var divStyle = this.state.isColap ? {overflow:"hidden", height:109} : {};
+        
         let timelineTopStyle = {
             backgroundColor: 'rgba(0,0,0,0.8)', 
             height: 110, 
@@ -79,14 +111,14 @@ class NewTimeline extends Component {
             padding: 10
         };
 
-        if (this.state.istimelineLoading) {
-            return <h2>Loading...</h2>;
-        }
-        else{
+        // if (this.state.istimelineLoading) {
+        //     return <h2>Loading...</h2>;
+        // }
+        // else{
             
             return(
                 
-                <div style={{backgroundColor: '#f4f6f8', paddingTop: 10}}> 
+        //         <div style={{backgroundColor: '#f4f6f8', paddingTop: 10}}> 
                     <div style={timelineTopStyle}>
                         <h1 style={{color: 'white', textAlign: 'center'}}>
                             <span style={{color: 'white'}}>
@@ -101,81 +133,55 @@ class NewTimeline extends Component {
                         <p style={{color: 'white', fontSize: 12, textAlign: 'center', marginBottom: 1}}>Ordered On: 18-01-2018</p>
                     
                     </div>
-                    <div style={{paddingLeft: 30}}>
-                        <Timeline>
-                        {this.state.timeline.items.map((stage, index) => {
+            );
+        //             <div style={{paddingLeft: 30}}>
+        //                 <Timeline>
+        //                 {this.state.timeline.items.map((stage, index) => {
 
-                            let titleText = "0"+(index+1)+". "+stage.title;
-                            let descriptionText = stage.description;
+        //                     let titleText = "0"+(index+1)+". "+stage.title;
+        //                     let descriptionText = stage.description;
 
-                            var ico = (<svg height="50" width="35" >
-                                            <image width="32" height="50" xlinkHref={stage.icon}  />    
-                                        </svg>);
+        //                     var ico = (<svg height="50" width="35" >
+        //                                     <image width="32" height="50" xlinkHref={stage.icon}  />    
+        //                                 </svg>);
                                         
 
-                            var stageData = stage.data;
+        //                     var stageData = stage.data;
 
-                            return(
-                                <TimelineEvent
-                                    key={index}
-                                    title={titleText}
-                                    titleStyle={{fontSize:15, fontWeight: "bold"}}
-                                    subtitle={descriptionText}
-                                    subtitleStyle={{fontSize:15}}
-                                    icon={ico}
-                                    contentStyle={{fontSize:13}}
-                                    bubbleStyle={{border: "none"}}
-                                    // onClick={this.showMessage}
-                                ><div id={index}>
+        //                     return(
+        //                         <TimelineEvent
+        //                             key={index}
+        //                             title={titleText}
+        //                             titleStyle={{fontSize:15, fontWeight: "bold"}}
+        //                             subtitle={descriptionText}
+        //                             subtitleStyle={{fontSize:15}}
+        //                             icon={ico}
+        //                             contentStyle={{fontSize:13}}
+        //                             bubbleStyle={{border: "none"}}
+        //                             // onClick={this.showMessage}
+        //                         ><div id={index}>
                                     
-                                    <TimelineContent 
-                                        collapseArray={this.state.array} 
-                                        collapseArrayKey={index} 
-                                        data={stageData}
-                                        componentID={"component"+index} 
-                                        onClick={this.handleClick}
-                                    />
+        //                             <TimelineContent 
+        //                                 collapseArray={this.state.array} 
+        //                                 collapseArrayKey={index} 
+        //                                 data={stageData}
+        //                                 componentID={"component"+index} 
+        //                                 onClick={this.handleClick}
+        //                             />
 
-                                    {/* { 
-                                        
-                                        Object.keys(stageData).map(function (key) {
-                                            if(isArray(stageData[key].value)) {   
-                                                var vals = stageData[key].value;
+                                    
 
-                                                return(
-                                                    <div key={key}>
-                                                        <span style={{fontWeight:'bold', fontSize: 14, color:'green'}}>
-                                                                {stageData[key].title} :
-                                                        </span>
-                                                        {
-                                                            Object.keys(vals).map(function (key) {
-                                                                
-                                                                return(
-                                                                    <div><span style={{fontWeight: 'bold'}}>&nbsp;&nbsp;&nbsp;{vals[key].title}</span> : <span>{vals[key].value}</span></div>
-                                                                )
-                                                            })
-                                                        }
-                                                        
-                                                    </div>       
-                                                ); 
-                                                           
-                                            }
-                                            return <div key={key}> <span style={{fontWeight:'bold', fontSize: 14, color:'green'}}> {stageData[key].title} :</span> {stageData[key].value}</div>
-                                        })
-                                    }  */}
+        //                         </div>            
+        //                         </TimelineEvent>                                    
+        //                     );
 
-                                </div>            
-                                </TimelineEvent>                                    
-                            );
+        //                 })}
+        //                 </Timeline>
+        //             </div>     
+        //         </div>
+            // );    
 
-                        })}
-                        </Timeline>
-                    </div>     
-                </div>
-            );    
-
-        }
-
+        // }
     }
     
 }
